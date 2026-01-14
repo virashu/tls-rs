2b63de94957760ebc21cd6a8bc584423f9eb91c8
virashu <virashu@disroot.org>
2026-01-14 17:57:56 +0300

    feat(tls): split SecureContext; finish handshake; transfer ApplicationData

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::{Result, anyhow, bail};
 use asn1::{
-    DataElement,
-    object_identifiers::{rsassaPss, sha256WithRSAEncryption},
+    object_identifiers::{RSASSA_PSS, SHA256_WITH_RSA_ENCRYPTION},
     parse_der,
     pkcs1::RsaPrivateKey,
     pkcs8::PrivateKeyInfo,
@@ -19,7 +18,7 @@ use crypt::{
 use tls::{
     cipher_suite::TLS_AES_256_GCM_SHA384,
     error::TlsAlert,
-    hkdf::{derive_secret, hkdf_expand_label, hkdf_extract},
+    hkdf::hkdf_expand_label,
     record::{
         TlsCiphertext, TlsContent, TlsPlaintext,
         handshake::{
@@ -39,14 +38,16 @@ use std::{
     collections::HashMap,
     fs,
     io::{Read, Write},
-    marker::PhantomData,
     net::{TcpListener, TcpStream},
-    sync::atomic::{AtomicU64, Ordering},
 };
 
-use crate::organized_extensions::OrganizedClientExtensions;
+use crate::{
+    organized_extensions::OrganizedClientExtensions,
+    secure_context::{TlsApplicationSecureContext, TlsHandshakeSecureContext, Transcript},
+};
 
 mod organized_extensions;
+mod secure_context;
 
 const VERSION: u16 = 0x0304;
 
@@ -60,6 +61,7 @@ fn load_cert() -> X509TbsCertificate {
 
 fn load_rsa_keys() -> (PrivateKey, PublicKey) {
     let encoded = std::fs::read("key.der").unwrap();
+
     let data = parse_der(&encoded);
     let private_key_info = PrivateKeyInfo::from_data_element(&data).unwrap();
 
@@ -78,13 +80,6 @@ fn load_rsa_keys() -> (PrivateKey, PublicKey) {
     )
 }
 
-fn xor<const N: usize>(mut a: [u8; N], b: [u8; N]) -> [u8; N] {
-    for i in 0..N {
-        a[i] ^= b[i];
-    }
-    a
-}
-
 struct ClientHelloInfo {
     legacy_session_id: Box<[u8]>,
 
@@ -97,106 +92,7 @@ struct ClientHelloInfo {
     server_share: Option<KeyShareEntry>,
 }
 
-struct TlsSecureContext<H: Hasher> {
-    seq_nonce: AtomicU64,
-
-    transcript: Vec<u8>,
-
-    server_handshake_traffic: [u8; 48],
-    server_handshake_key: [u8; 32],
-    server_handshake_iv: [u8; 12],
-
-    client_handshake_key: [u8; 32],
-    client_handshake_iv: [u8; 12],
-
-    _hash: PhantomData<H>,
-}
-
-impl<H: Hasher> TlsSecureContext<H> {
-    pub fn new(
-        key_ecdhe: Option<&[u8]>,
-        key_psk: Option<&[u8]>,
-        transcript: Vec<u8>,
-    ) -> Result<Self> {
-        // Key Schedule
-        let early_secret = hkdf_extract::<H>(&[0; 48], key_psk.unwrap_or(&[0; 48]));
-        let handshake_secret = hkdf_extract::<H>(
-            &derive_secret::<H>(&early_secret, "derived", &[]),
-            key_ecdhe.unwrap_or(&[0; 32]),
-        );
-
-        // Server keys
-        let server_handshake_traffic: [u8; 48] =
-            derive_secret::<H>(&handshake_secret, "s hs traffic", &transcript)
-                .as_ref()
-                .try_into()?;
-        let server_handshake_key: [u8; 32] =
-            hkdf_expand_label::<H>(&server_handshake_traffic, "key", &[], 32)
-                .as_ref()
-                .try_into()?;
-        let server_handshake_iv: [u8; 12] =
-            hkdf_expand_label::<H>(&server_handshake_traffic, "iv", &[], 12)
-                .as_ref()
-                .try_into()?;
-
-        // Client keys
-        let client_handshake_traffic_secret: [u8; 48] =
-            derive_secret::<H>(&handshake_secret, "c hs traffic", &transcript)
-                .as_ref()
-                .try_into()?;
-        let client_handshake_key: [u8; 32] =
-            hkdf_expand_label::<H>(&client_handshake_traffic_secret, "key", &[], 32)
-                .as_ref()
-                .try_into()?;
-        let client_handshake_iv: [u8; 12] =
-            hkdf_expand_label::<H>(&client_handshake_traffic_secret, "iv", &[], 12)
-                .as_ref()
-                .try_into()?;
-
-        // Main
-        let main_secret = hkdf_extract::<H>(
-            &derive_secret::<H>(&handshake_secret, "derived", &[]),
-            &[0; 48],
-        );
-
-        Ok(Self {
-            seq_nonce: AtomicU64::new(0),
-            transcript,
-            server_handshake_traffic,
-            server_handshake_key,
-            server_handshake_iv,
-            client_handshake_key,
-            client_handshake_iv,
-            _hash: PhantomData,
-        })
-    }
-
-    pub fn nonce(&self) -> u64 {
-        self.seq_nonce.fetch_add(1, Ordering::Relaxed)
-    }
-
-    pub fn pad_nonce<const L: usize>(&self) -> [u8; L] {
-        let mut x = [0; L];
-        x[(L - 8)..L].copy_from_slice(&self.nonce().to_be_bytes());
-        x
-    }
-
-    pub fn extend_transcript(&mut self, value: &[u8]) {
-        self.transcript.extend(value);
-    }
-
-    pub fn transcript_hash(&self) -> Box<[u8]> {
-        H::hash(&self.transcript)
-    }
-
-    pub fn encrypt(&mut self, plaintext: &TlsPlaintext) -> Result<TlsCiphertext> {
-        self.transcript.extend(&plaintext.to_raw()[5..]);
-        let nonce = xor(self.pad_nonce(), self.server_handshake_iv);
-        TlsCiphertext::encrypt(plaintext, self.server_handshake_key, nonce)
-    }
-}
-
-fn server_hello(client_info: ClientHelloInfo) -> Result<Box<[u8]>> {
+fn server_hello(client_info: ClientHelloInfo) -> Result<TlsPlaintext> {
     let mut sh_extensions = Vec::from([ServerHelloExtension::new_supported_versions(VERSION)]);
 
     if let Some(share) = client_info.server_share {
@@ -214,44 +110,41 @@ fn server_hello(client_info: ClientHelloInfo) -> Result<Box<[u8]>> {
         &sh_extensions,
     ));
     let sh_record = TlsPlaintext::new_handshake(server_hello)?;
-    Ok(sh_record.to_raw())
+    Ok(sh_record)
 }
 
-fn handshake(conn: &mut TcpStream) -> Result<()> {
+fn handshake(conn: &mut TcpStream) -> Result<TlsApplicationSecureContext<Sha384>> {
     let mut buf = [0; 2800];
     let n = conn.read(&mut buf)?;
 
-    let mut transcript = Vec::<u8>::new();
+    let mut transcript = Transcript::<Sha384>::new();
 
     // ClientHello
 
     let ch_raw = &buf[..n];
-    transcript.extend(&ch_raw[5..]);
     let ch_record = TlsPlaintext::from_raw(ch_raw)?;
+    transcript.extend_raw(&ch_raw[5..]);
     let TlsContent::Handshake(Handshake::ClientHello(client_hello)) = ch_record.fragment else {
         bail!("Not client hello");
     };
     let ch_exts = OrganizedClientExtensions::organize(client_hello.extensions);
 
-    // EC-DHE
+    // ECDHE
 
     let key_share = ch_exts
         .key_share
         .ok_or(anyhow!("Missing key_share"))?
         .to_hashmap();
 
-    let x25519_public;
-    let x25519_shared;
-
-    if let Some(share) = key_share.get(&NamedGroup::x25519) {
-        let (public, private) = x25519::get_keypair();
-
-        x25519_public = Some(public);
-        x25519_shared = Some(x25519::get_shared_key(private, share.as_ref().try_into()?));
-    } else {
-        x25519_public = None;
-        x25519_shared = None;
-    }
+    let (x25519_public, x25519_shared) = key_share
+        .get(&NamedGroup::x25519)
+        .map(|share| -> Result<_> {
+            let (public, private) = x25519::get_keypair();
+            let shared = x25519::get_shared_key(private, share.as_ref().try_into()?);
+            Ok((public, shared))
+        })
+        .transpose()?
+        .unzip();
 
     // ServerHello
 
@@ -264,25 +157,26 @@ fn handshake(conn: &mut TcpStream) -> Result<()> {
         server_share,
     };
 
-    let sh_raw = server_hello(client_info)?;
-    transcript.extend(&sh_raw[5..]);
-    conn.write_all(&sh_raw)?;
+    let sh_record = server_hello(client_info)?;
+    transcript.extend(&sh_record);
+    conn.write_all(&sh_record.to_raw())?;
 
-    let mut context = TlsSecureContext::<Sha384>::new(
-        x25519_shared.as_ref().map(|x| x as &[u8]),
+    let context = TlsHandshakeSecureContext::<Sha384>::new(
+        x25519_shared.as_ref().map(|x| x.as_slice()),
         None,
-        transcript,
+        transcript.hash(),
     )?;
 
-    // EncryptedExtensions
+    // Server EncryptedExtensions
     {
         let record = TlsPlaintext::new_handshake(Handshake::EncryptedExtensions(
-            EncryptedExtensions::new(&[])?,
+            EncryptedExtensions::new(&[ServerHelloExtension::new_alpn(b"http/1.1")?])?,
         ))?;
-        conn.write_all(&context.encrypt(&record)?.to_raw())?;
+        transcript.extend(&record);
+        conn.write_all(&context.encrypt_server(&record)?.to_raw())?;
     }
 
-    // Certificate
+    // Server Certificate
     {
         let certificate = fs::read("cert.cer")?;
 
@@ -290,24 +184,25 @@ fn handshake(conn: &mut TcpStream) -> Result<()> {
             &[],
             &[CertificateEntry::new(&certificate)?],
         )?))?;
-        conn.write_all(&context.encrypt(&record)?.to_raw())?;
+        transcript.extend(&record);
+        conn.write_all(&context.encrypt_server(&record)?.to_raw())?;
     }
 
     // Determine certificate type
     let cert = load_cert();
-    let signature_scheme = if cert.signature_algorithm.is(sha256WithRSAEncryption) {
+    let signature_scheme = if cert.signature_algorithm.is(SHA256_WITH_RSA_ENCRYPTION) {
         tracing::info!("Using RSAE");
         SignatureScheme::rsa_pss_rsae_sha256
-    } else if cert.signature_algorithm.is(rsassaPss) {
+    } else if cert.signature_algorithm.is(RSASSA_PSS) {
         tracing::info!("Using RSASSA-PSS");
         SignatureScheme::rsa_pss_pss_sha256
     } else {
         unimplemented!();
     };
 
-    // CertificateVerify
+    // Server CertificateVerify
     {
-        let transcript_hash = context.transcript_hash();
+        let transcript_hash = transcript.hash();
         let sign_context = concat_dyn![
             [0x20].repeat(64),
             b"TLS 1.3, server CertificateVerify",
@@ -315,46 +210,82 @@ fn handshake(conn: &mut TcpStream) -> Result<()> {
             transcript_hash,
         ];
         let (private_key, public_key) = load_rsa_keys();
-        let signature = crypt::rsa::rsassa_pss_sign::<Sha256, { Sha256::DIGEST_SIZE }>(
-            &private_key,
-            &sign_context,
-        );
+        let signature = loop {
+            let signature = crypt::rsa::rsassa_pss_sign::<Sha256, { Sha256::DIGEST_SIZE }>(
+                &private_key,
+                &sign_context,
+            );
 
-        crypt::rsa::rsassa_pss_verify::<Sha256, { Sha256::DIGEST_SIZE }>(
-            &public_key,
-            &sign_context,
-            &signature,
-        )
-        .unwrap();
+            match crypt::rsa::rsassa_pss_verify::<Sha256, { Sha256::DIGEST_SIZE }>(
+                &public_key,
+                &sign_context,
+                &signature,
+            ) {
+                Ok(()) => break signature,
+                Err(e) => tracing::error!("{e}"),
+            }
+        };
 
         let record = TlsPlaintext::new_handshake(Handshake::CertificateVerify(
             CertificateVerify::new(signature_scheme, &signature)?,
         ))?;
-        conn.write_all(&context.encrypt(&record)?.to_raw())?;
+        transcript.extend(&record);
+        conn.write_all(&context.encrypt_server(&record)?.to_raw())?;
     }
 
-    // Finished
+    // Server Finished
     {
         let finished_key =
-            hkdf_expand_label::<Sha384>(&context.server_handshake_traffic, "finished", &[], 48);
-        let verify_data = hmac_hash::<Sha384>(&finished_key, &context.transcript_hash());
+            hkdf_expand_label::<Sha384, 48>(&context.server_handshake_secret, "finished", &[])?;
+        let verify_data = hmac_hash::<Sha384>(&finished_key, &transcript.hash());
 
         let record = TlsPlaintext::new_handshake(Handshake::Finished(Finished { verify_data }))?;
-        conn.write_all(&context.encrypt(&record)?.to_raw())?;
+        transcript.extend(&record);
+        conn.write_all(&context.encrypt_server(&record)?.to_raw())?;
     }
 
-    Ok(())
+    let app_context =
+        TlsApplicationSecureContext::<Sha384>::new(context.handshake_secret, transcript.hash())?;
+
+    // # Client
+
+    loop {
+        let n = conn.read(&mut buf)?;
+        let record = TlsPlaintext::from_raw(&buf[..n])?;
+
+        if matches!(record.fragment, TlsContent::ApplicationData(_)) {
+            let ciphertext = TlsCiphertext::from_raw(&buf[..n])?;
+            let record = context.decrypt_client(&ciphertext)?;
+
+            tracing::trace!(target: "(IN) (ENCRYPTED)", ?record);
+
+            if matches!(
+                record.fragment,
+                TlsContent::Handshake(Handshake::Finished(_))
+            ) {
+                break;
+            }
+        } else {
+            tracing::trace!(target: "(IN)", ?record);
+        }
+    }
+
+    tracing::info!("Handshake: Done");
+
+    Ok(app_context)
 }
 
 fn handle_connection(mut conn: TcpStream) -> Result<()> {
-    if let Err(e) = handshake(&mut conn) {
-        match e.downcast::<TlsAlert>() {
+    let context = match handshake(&mut conn) {
+        Ok(c) => c,
+        Err(e) => match e.downcast::<TlsAlert>() {
             Ok(alert) => {
                 tracing::warn!("Alert: {alert:?}");
+                return Ok(());
             }
             Err(e) => return Err(e),
-        }
-    }
+        },
+    };
 
     let mut buf = [0; 2800];
     loop {
@@ -365,16 +296,27 @@ fn handle_connection(mut conn: TcpStream) -> Result<()> {
         }
 
         tracing::info!("Read {n} bytes");
-        let record = TlsPlaintext::from_raw(&buf[..n])?;
 
-        tracing::info!(?record);
+        if let Ok(ciphertext) = TlsCiphertext::from_raw(&buf[..n]) {
+            let record = context.decrypt_client(&ciphertext)?;
+
+            if let TlsContent::ApplicationData(data) = record.fragment {
+                let st = data.to_string();
+                tracing::info!(?st);
+            } else {
+                tracing::trace!(target: "(IN)", ?record);
+            }
+        }
     }
 
     // Ok(())
 }
 
 fn main() -> Result<()> {
-    tracing_subscriber::fmt().with_env_filter("trace").init();
+    tracing_subscriber::fmt()
+        .with_env_filter("trace")
+        .pretty()
+        .init();
 
     let listener = TcpListener::bind("0.0.0.0:3001")?;
 
---
+++ src/secure_context.rs
@@ -0,0 +1,346 @@
+use anyhow::Result;
+use crypt::hash::Hasher;
+use tls::{
+    hkdf::{hkdf_expand_label, hkdf_extract},
+    record::{TlsCiphertext, TlsPlaintext},
+};
+
+use std::{
+    marker::PhantomData,
+    sync::atomic::{AtomicU64, Ordering},
+};
+
+const H_LENGTH: usize = 48; // Sha384
+const KEY_LENGTH: usize = 32;
+const IV_LENGTH: usize = 12;
+
+fn xor<const N: usize>(mut a: [u8; N], b: [u8; N]) -> [u8; N] {
+    for i in 0..N {
+        a[i] ^= b[i];
+    }
+    a
+}
+
+pub struct Transcript<H: Hasher> {
+    inner: Vec<u8>,
+    _hash: PhantomData<H>,
+}
+
+impl<H: Hasher> Transcript<H> {
+    pub fn new() -> Self {
+        Self {
+            inner: Vec::new(),
+            _hash: PhantomData,
+        }
+    }
+
+    pub fn extend(&mut self, plaintext: &TlsPlaintext) {
+        self.inner.extend(&plaintext.to_raw()[5..]);
+    }
+
+    pub fn extend_raw(&mut self, raw: &[u8]) {
+        self.inner.extend(raw);
+    }
+
+    pub fn hash(&self) -> [u8; H_LENGTH] {
+        H::hash(&self.inner).as_ref().try_into().unwrap()
+    }
+}
+
+pub struct TlsHandshakeSecureContext<H: Hasher> {
+    seq_server_nonce: AtomicU64,
+    seq_client_nonce: AtomicU64,
+
+    pub handshake_secret: [u8; H_LENGTH],
+
+    pub server_handshake_secret: [u8; H_LENGTH],
+    pub server_handshake_key: [u8; KEY_LENGTH],
+    pub server_handshake_iv: [u8; IV_LENGTH],
+
+    pub client_handshake_secret: [u8; H_LENGTH],
+    pub client_handshake_key: [u8; KEY_LENGTH],
+    pub client_handshake_iv: [u8; IV_LENGTH],
+
+    _hash: PhantomData<H>,
+}
+
+impl<H: Hasher> TlsHandshakeSecureContext<H> {
+    pub fn new(
+        key_ecdhe: Option<&[u8]>,
+        key_psk: Option<&[u8]>,
+        transcript_hash: [u8; H_LENGTH],
+    ) -> Result<Self> {
+        let empty_hash = H::hash(&[]);
+
+        let early_secret = hkdf_extract::<H>(&[0; H_LENGTH], key_psk.unwrap_or(&[0; H_LENGTH]));
+        let derived_secret =
+            hkdf_expand_label::<H, H_LENGTH>(&early_secret, "derived", &empty_hash)?;
+        let handshake_secret: [u8; H_LENGTH] =
+            hkdf_extract::<H>(&derived_secret, key_ecdhe.unwrap_or(&[0; 32]))
+                .as_ref()
+                .try_into()?;
+
+        // ## Server
+        let server_handshake_secret =
+            hkdf_expand_label::<H, H_LENGTH>(&handshake_secret, "s hs traffic", &transcript_hash)?;
+        let server_handshake_key =
+            hkdf_expand_label::<H, KEY_LENGTH>(&server_handshake_secret, "key", &[])?;
+        let server_handshake_iv =
+            hkdf_expand_label::<H, IV_LENGTH>(&server_handshake_secret, "iv", &[])?;
+
+        // ## Client
+        let client_handshake_secret =
+            hkdf_expand_label::<H, H_LENGTH>(&handshake_secret, "c hs traffic", &transcript_hash)?;
+        let client_handshake_key =
+            hkdf_expand_label::<H, KEY_LENGTH>(&client_handshake_secret, "key", &[])?;
+        let client_handshake_iv =
+            hkdf_expand_label::<H, IV_LENGTH>(&client_handshake_secret, "iv", &[])?;
+
+        Ok(Self {
+            seq_server_nonce: AtomicU64::new(0),
+            seq_client_nonce: AtomicU64::new(0),
+            handshake_secret,
+            server_handshake_secret,
+            server_handshake_key,
+            server_handshake_iv,
+            client_handshake_secret,
+            client_handshake_key,
+            client_handshake_iv,
+            _hash: PhantomData,
+        })
+    }
+
+    pub fn server_nonce<const L: usize>(&self) -> [u8; L] {
+        let nonce = self.seq_server_nonce.fetch_add(1, Ordering::Relaxed);
+
+        let mut x = [0; L];
+        x[(L - 8)..L].copy_from_slice(&nonce.to_be_bytes());
+        x
+    }
+
+    pub fn client_nonce<const L: usize>(&self) -> [u8; L] {
+        let nonce = self.seq_client_nonce.fetch_add(1, Ordering::Relaxed);
+
+        let mut x = [0; L];
+        x[(L - 8)..L].copy_from_slice(&nonce.to_be_bytes());
+        x
+    }
+
+    pub fn encrypt_server(&self, plaintext: &TlsPlaintext) -> Result<TlsCiphertext> {
+        let nonce = xor(self.server_nonce(), self.server_handshake_iv);
+        TlsCiphertext::encrypt(plaintext, self.server_handshake_key, nonce)
+    }
+
+    pub fn decrypt_client(&self, ciphertext: &TlsCiphertext) -> Result<TlsPlaintext> {
+        let nonce = xor(self.client_nonce(), self.client_handshake_iv);
+        let plaintext = ciphertext.decrypt(self.client_handshake_key, nonce)?;
+        Ok(plaintext)
+    }
+}
+
+pub struct TlsApplicationSecureContext<H: Hasher> {
+    seq_server_nonce: AtomicU64,
+    seq_client_nonce: AtomicU64,
+
+    server_application_traffic: [u8; H_LENGTH],
+    server_application_key: [u8; KEY_LENGTH],
+    server_application_iv: [u8; IV_LENGTH],
+
+    client_application_traffic: [u8; H_LENGTH],
+    client_application_key: [u8; KEY_LENGTH],
+    client_application_iv: [u8; IV_LENGTH],
+
+    _hash: PhantomData<H>,
+}
+
+impl<H: Hasher> TlsApplicationSecureContext<H> {
+    pub fn new(handshake_secret: [u8; H_LENGTH], transcript_hash: [u8; H_LENGTH]) -> Result<Self> {
+        let empty_hash = H::hash(&[]);
+
+        let derived_secret =
+            hkdf_expand_label::<H, H_LENGTH>(&handshake_secret, "derived", &empty_hash)?;
+        let main_secret = hkdf_extract::<H>(&derived_secret, &[0; H_LENGTH]);
+
+        // ## Server
+        let server_application_traffic =
+            hkdf_expand_label::<H, H_LENGTH>(&main_secret, "s ap traffic", &transcript_hash)?;
+        let server_application_key =
+            hkdf_expand_label::<H, KEY_LENGTH>(&server_application_traffic, "key", &[])?;
+        let server_application_iv =
+            hkdf_expand_label::<H, IV_LENGTH>(&server_application_traffic, "iv", &[])?;
+
+        // ## Client
+        let client_application_traffic =
+            hkdf_expand_label::<H, H_LENGTH>(&main_secret, "c ap traffic", &transcript_hash)?;
+        let client_application_key =
+            hkdf_expand_label::<H, KEY_LENGTH>(&client_application_traffic, "key", &[])?;
+        let client_application_iv =
+            hkdf_expand_label::<H, IV_LENGTH>(&client_application_traffic, "iv", &[])?;
+
+        Ok(Self {
+            seq_server_nonce: AtomicU64::new(0),
+            seq_client_nonce: AtomicU64::new(0),
+            server_application_traffic,
+            server_application_key,
+            server_application_iv,
+            client_application_traffic,
+            client_application_key,
+            client_application_iv,
+            _hash: PhantomData,
+        })
+    }
+
+    pub fn server_nonce<const L: usize>(&self) -> [u8; L] {
+        let nonce = self.seq_server_nonce.fetch_add(1, Ordering::Relaxed);
+
+        let mut x = [0; L];
+        x[(L - 8)..L].copy_from_slice(&nonce.to_be_bytes());
+        x
+    }
+
+    pub fn client_nonce<const L: usize>(&self) -> [u8; L] {
+        let nonce = self.seq_client_nonce.fetch_add(1, Ordering::Relaxed);
+
+        let mut x = [0; L];
+        x[(L - 8)..L].copy_from_slice(&nonce.to_be_bytes());
+        x
+    }
+
+    pub fn update_keys(&mut self) -> Result<()> {
+        // ## Server
+        self.server_application_traffic =
+            hkdf_expand_label::<H, H_LENGTH>(&self.server_application_traffic, "traffic upd", &[])?;
+        self.server_application_key =
+            hkdf_expand_label::<H, KEY_LENGTH>(&self.server_application_traffic, "key", &[])?;
+        self.server_application_iv =
+            hkdf_expand_label::<H, IV_LENGTH>(&self.server_application_traffic, "iv", &[])?;
+
+        // ## Client
+        self.client_application_traffic =
+            hkdf_expand_label::<H, H_LENGTH>(&self.client_application_traffic, "traffic upd", &[])?;
+        self.client_application_key =
+            hkdf_expand_label::<H, KEY_LENGTH>(&self.client_application_traffic, "key", &[])?;
+        self.client_application_iv =
+            hkdf_expand_label::<H, IV_LENGTH>(&self.client_application_traffic, "iv", &[])?;
+
+        Ok(())
+    }
+
+    pub fn decrypt_client(&self, ciphertext: &TlsCiphertext) -> Result<TlsPlaintext> {
+        let nonce = xor(self.client_nonce(), self.client_application_iv);
+        let plaintext = ciphertext.decrypt(self.client_application_key, nonce)?;
+        Ok(plaintext)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crypt::hash::sha::Sha384;
+    use hex_literal::hex;
+
+    use super::*;
+
+    #[test]
+    fn test_handshake() {
+        let transcript_hash = hex!(
+            "e05f64fcd082bdb0dce473adf669c276
+             9f257a1c75a51b7887468b5e0e7a7de4
+             f4d34555112077f16e079019d5a845bd"
+        );
+        let shared_secret = hex!(
+            "df4a291baa1eb7cfa6934b29b474baad
+             2697e29f1f920dcc77c8a0a088447624"
+        );
+
+        let context =
+            TlsHandshakeSecureContext::<Sha384>::new(Some(&shared_secret), None, transcript_hash)
+                .unwrap();
+
+        assert_eq!(
+            context.handshake_secret,
+            hex!(
+                "bdbbe8757494bef20de932598294ea65
+                 b5e6bf6dc5c02a960a2de2eaa9b07c92
+                 9078d2caa0936231c38d1725f179d299"
+            )
+        );
+        assert_eq!(
+            context.server_handshake_secret,
+            hex!(
+                "23323da031634b241dd37d61032b62a4
+                 f450584d1f7f47983ba2f7cc0cdcc39a
+                 68f481f2b019f9403a3051908a5d1622"
+            )
+        );
+        assert_eq!(
+            context.server_handshake_key,
+            hex!(
+                "9f13575ce3f8cfc1df64a77ceaffe897
+                 00b492ad31b4fab01c4792be1b266b7f"
+            )
+        );
+        assert_eq!(
+            context.server_handshake_iv,
+            hex!("9563bc8b590f671f488d2da3")
+        );
+        assert_eq!(
+            context.client_handshake_secret,
+            hex!(
+                "db89d2d6df0e84fed74a2288f8fd4d09
+                 59f790ff23946cdf4c26d85e51bebd42
+                 ae184501972f8d30c4a3e4a3693d0ef0"
+            )
+        );
+        assert_eq!(
+            context.client_handshake_key,
+            hex!(
+                "1135b4826a9a70257e5a391ad93093df
+                 d7c4214812f493b3e3daae1eb2b1ac69"
+            )
+        );
+        assert_eq!(
+            context.client_handshake_iv,
+            hex!("4256d2e0e88babdd05eb2f27")
+        );
+    }
+
+    #[test]
+    fn test_application() {
+        let transcript_hash = hex!(
+            "fa6800169a6baac19159524fa7b9721b
+             41be3c9db6f3f93fa5ff7e3db3ece204
+             d2b456c51046e40ec5312c55a86126f5"
+        );
+        let handshake_secret = hex!(
+            "bdbbe8757494bef20de932598294ea65
+             b5e6bf6dc5c02a960a2de2eaa9b07c92
+             9078d2caa0936231c38d1725f179d299"
+        );
+
+        let context =
+            TlsApplicationSecureContext::<Sha384>::new(handshake_secret, transcript_hash).unwrap();
+
+        assert_eq!(
+            context.server_application_key,
+            hex!(
+                "01f78623f17e3edcc09e944027ba3218
+                 d57c8e0db93cd3ac419309274700ac27"
+            )
+        );
+        assert_eq!(
+            context.server_application_iv,
+            hex!("196a750b0c5049c0cc51a541")
+        );
+        assert_eq!(
+            context.client_application_key,
+            hex!(
+                "de2f4c7672723a692319873e5c227606
+                 691a32d1c59d8b9f51dbb9352e9ca9cc"
+            )
+        );
+        assert_eq!(
+            context.client_application_iv,
+            hex!("bb007956f474b25de902432f")
+        );
+    }
+}
